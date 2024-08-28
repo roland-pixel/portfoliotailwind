9373d47e1cf97176dc8531f0823e1f425545f97f
roland-pixel <kharis7181@gmail.com>
2024-08-28 22:01:34 +0700

    update darkmode + perbaikan fitur

--- src/script.js
+++ src/script.js
@@ -1,21 +1,55 @@
 // navbar fixed
-window.onscroll = function(){
-    const  header = document.querySelector('header');
-    const fixedNav = header.offsetTop;
+window.onscroll = function () {
+  const header = document.querySelector("header");
+  const fixedNav = header.offsetTop;
+  const totop = document.querySelector("#to-top");
 
-    if(window.pageYOffset > fixedNav){
-        header.classList.add('navbar-fixed');
-    }else{
-        header.classList.remove('navbar-fixed');
-    }
-}
-
+  if (window.pageYOffset > fixedNav) {
+    header.classList.add("navbar-fixed");
+    totop.classList.remove("hidden");
+    totop.classList.add("flex");
+  } else {
+    totop.classList.remove("flex");
+    totop.classList.add("hidden");
+    header.classList.remove("navbar-fixed");
+  }
+};
 
 // Hamburger
-const hamburger = document.querySelector('#hamburger');
-const navMenu = document.querySelector('#nav-menu');
+const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("#nav-menu");
 
-hamburger.addEventListener('click', function(){
-    hamburger.classList.toggle('hamburger-active');
-    navMenu.classList.toggle('hidden');
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("hamburger-active");
+  navMenu.classList.toggle("hidden");
 });
+
+// klik diluar hamburger
+window.addEventListener("click", function (e) {
+  if (e.target != hamburger && e.target != navMenu) {
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.add("hidden");
+  }
+});
+
+// darkmode toggle
+const darktoggle = document.querySelector('#dark-toggle');
+const html = document.querySelector('html');
+
+darktoggle.addEventListener('click', function(){
+    if (darktoggle.checked){
+        html.classList.add('dark');
+        localStorage.theme = 'dark'
+    }else{
+        html.classList.remove('dark');
+        localStorage.theme = 'light';
+    }
+});
+
+
+// pindahkan posisi toggle sesuai mode
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    darktoggle.checked = true;
+} else {
+      darktoggle.checked = false;
+  }
